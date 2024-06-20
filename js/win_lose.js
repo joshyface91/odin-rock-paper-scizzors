e0643f1358496aee08bba3105c732f9d519d3ed2
joshyface91 <46246044+joshyface91@users.noreply.github.com>
2024-06-19 18:18:49 -0600

    Update win_lose.js

--- js/win_lose.js
+++ js/win_lose.js
@@ -6,7 +6,7 @@ let tallyComputer = document.querySelector("#tally-computer");
 window.onload = function () {
     const returnHome = document.querySelector(".logo");
     returnHome.addEventListener("click", () => {
-        window.location = "/";
+        window.location = "odin-rock-paper-scizzors/";
     });
     
     let finalMessage = sessionStorage.getItem("finalMessage");
